973f35b1616072abb00b3356c1652692a7363e7f
Aaron <a.rcottrill521@gmail.com>
2021-02-04 12:15:38 -0500

    Just needs to be styled and tested

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ import React from 'react';
 //sounds 
 import heater1 from "./audio/Heater-1.mp3";
 import heater2 from "./audio/Heater-2.mp3";
-
+import heater3 from "./audio/Heater-3.mp3";
+import heater4 from "./audio/Heater-4_1.mp3";
+import heater6 from "./audio/Heater-6.mp3";
+import kickNHat from "./audio/Kick_n_Hat.mp3";
+import disco from "./audio/Dsc_Oh.mp3";
+import cev from "./audio/Cev_H2.mp3"
+import rp4 from "./audio/RP4_KICK_1.mp3"
 
 class App extends React.Component {
   constructor(props) {
@@ -23,6 +29,7 @@ class App extends React.Component {
    * @param {KeyboardEvent} evt 
    */
   keyPress(event) {
+    //Have to remove the "Key" prefix
     const key = event.code.substring(3); // "a", "1", "Shift", etc.
     if (key === "Q") {
       let audioElement = document.getElementById('Q');
@@ -30,6 +37,34 @@ class App extends React.Component {
     }
     else if (key === "W") {
       let audioElement = document.getElementById('W');
+      audioElement.play();
+    }
+    else if (key === "W") {
+      let audioElement = document.getElementById('W');
+      audioElement.play();
+    }
+    else if (key === "E") {
+      let audioElement = document.getElementById('E');
+      audioElement.play();
+    }
+    else if (key === "A") {
+      let audioElement = document.getElementById('A');
+      audioElement.play();
+    }
+    else if (key === "S") {
+      let audioElement = document.getElementById('S');
+      audioElement.play();
+    }
+    else if (key === "D") {
+      let audioElement = document.getElementById('D');
+      audioElement.play();
+    }
+    else if (key === "Z") {
+      let audioElement = document.getElementById('Z');
+      audioElement.play();
+    }
+    else if (key === "X") {
+      let audioElement = document.getElementById('X');
       audioElement.play();
     }
   }
@@ -61,7 +96,34 @@ class App extends React.Component {
           <audio id="W" src={heater2} />
           W
         </button>
-
+        <button id="Heater-3" className="drum-pad" onClick={this.hitDrumButton}>
+          <audio id="E" src={heater3} />
+          E
+        </button>
+        <button id="Heater-4_1" className="drum-pad" onClick={this.hitDrumButton}>
+          <audio id="A" src={heater4} />
+          A
+        </button>
+        <button id="Kick_n_Hat" className="drum-pad" onClick={this.hitDrumButton}>
+          <audio id="S" src={kickNHat} />
+          S
+        </button>
+        <button id="Dsc_Oh" className="drum-pad" onClick={this.hitDrumButton}>
+          <audio id="D" src={disco} />
+          D
+        </button>
+        <button id="Heater-6" className="drum-pad" onClick={this.hitDrumButton}>
+          <audio id="Z" src={heater6} />
+          Z
+        </button>
+        <button id="RP4_KICK_1" className="drum-pad" onClick={this.hitDrumButton}>
+          <audio id="X" src={rp4} />
+          X
+        </button>
+        <button id="Cev_H2" className="drum-pad" onClick={this.hitDrumButton}>
+          <audio id="C" src={cev} />
+          C
+        </button>
       </div>
     );
   }
