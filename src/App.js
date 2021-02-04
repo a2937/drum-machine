0b12a0624e754003983ac28de6263469cc485626
Aaron <a.rcottrill521@gmail.com>
2021-02-03 20:26:19 -0500

    Add 2 functioning buttons.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//sounds 
+import heater1 from "./audio/Heater-1.mp3";
+import heater2 from "./audio/Heater-2.mp3";
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.hitDrumButton = this.hitDrumButton.bind(this);
+
+  }
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.keyPress);
+  }
+
+  /**
+   * 
+   * @param {KeyboardEvent} evt 
+   */
+  keyPress(event) {
+    const key = event.code.substring(3); // "a", "1", "Shift", etc.
+    if (key === "Q") {
+      let audioElement = document.getElementById('Q');
+      audioElement.play();
+    }
+    else if (key === "W") {
+      let audioElement = document.getElementById('W');
+      audioElement.play();
+    }
+  }
+
+  /**
+   * 
+   * @param {React.MouseEvent<HTMLButtonElement, MouseEvent>} event
+   */
+  hitDrumButton(event) {
+    var target = event.target;
+    var element = document.getElementById(target.id);
+
+    var audioElement = element.firstElementChild;
+    audioElement.play();
+  }
+
+
+  render() {
+    return (
+      <div id="drum-machine" className="App" >
+        <p id="display"></p>
+
+        <button id="Heater-1" className="drum-pad" onClick={this.hitDrumButton}>
+          <audio id="Q" src={heater1} />
+          Q
+        </button>
+
+        <button id="Heater-2" className="drum-pad" onClick={this.hitDrumButton}>
+          <audio id="W" src={heater2} />
+          W
+        </button>
+
+      </div>
+    );
+  }
+
 }
 
 export default App;
